04301e7033f9acd3419473d65bb201f30451c7d0
everth333 <36476532+everth333@users.noreply.github.com>
2019-08-17 00:48:36 -0400

    Creacion de clases
    Se ha creado el proyecto vacio y su test.

---
+++ caGoL_amaya/caGoL_amaya/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace caGoL_amaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print();
+        }
+
+        public static void print()
+        {
+            System.Console.WriteLine("Hola mundo1");
+            System.Console.Read();
+        }
+
+    }
+}
