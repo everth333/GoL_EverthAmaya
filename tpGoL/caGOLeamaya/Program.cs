25151fc47a9439a4593598e38549fba11bc18c16
everth333 <36476532+everth333@users.noreply.github.com>
2019-08-17 00:55:11 -0400

    Iniciar projecto, clase y test
    Inicio del proyecto

---
+++ tpGoL/caGOLeamaya/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace caGOLeamaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            print();
+        }
+
+        public static void print()
+        {
+            System.Console.WriteLine("Hola Mundo");
+            System.Console.Read();
+        }
+
+    }
+}
---
+++ tpGoL/caGOLeamaya/clGoL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace caGOLeamaya
+{
+    public class clGoL
+    {
+
+        //
+
+        public clGoL()
+        {
+
+        }
+
+        public string Hello()
+        {
+            return "Hello";    
+        }
+
+
+        //
+
+
+    }
+}
